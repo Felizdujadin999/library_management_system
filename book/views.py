d08b54b24874ca38f8f82795969ff88190220fd4
Felizdujadin <ezeikefelix51@gmail.com>
2023-05-16 11:51:40 +0100

    uploaded new files

--- book/views.py
+++ book/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import render
 from book.models import Book, Author
-
+from .serializers import AuthorSerializer
 
 # Create your views here.
 
